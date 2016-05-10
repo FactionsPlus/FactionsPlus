8c7d08910bb73d70ef163f4cb894f947a411fe5f
Jonathanftw <romerojonathan280@gmail.com>
2016-05-09 20:47:28 -0700

    Update FactionsCommands.php

--- src/FactionsCommands.php
+++ src/FactionsCommands.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace FactionsPlus;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\math\Vector3;
+use pocketmine\level\level;
+use pocketmine\level\Position;
+class FactionsCommands {
+	
+	public $plugin;
+	
+	public function __construct(FactionsMain $pg) {
+		$this->plugin = $pg;
+	}
+	
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		if($sender instanceof Player) {
+			$player = $sender->getPlayer()->getName();
+			if(strtolower($command->getName('f'))) {
+				if(empty($args)) {
+					$sender->sendMessage($this->plugin->formatMessage("Please use /f help for a list of commands"));
+					return true;
